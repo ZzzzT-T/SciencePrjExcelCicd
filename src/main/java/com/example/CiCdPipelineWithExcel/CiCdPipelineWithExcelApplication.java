441d687de33ba650cca92da35e3fc5e9643dc742
ZzzzT-T <opps_zhen@yahoo.com>
2025-06-20 23:11:44 +0800

    Initial Commit

---
+++ src/main/java/com/example/CiCdPipelineWithExcel/CiCdPipelineWithExcelApplication.java
@@ -0,0 +1,13 @@
+package com.example.CiCdPipelineWithExcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdPipelineWithExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdPipelineWithExcelApplication.class, args);
+	}
+
+}
